71ad64e232c4d8737fb5fe27f88670b07e53130b
elaine460 <102460032+elaine460@users.noreply.github.com>
2024-12-14 21:12:56 +0800

    factorial

--- src/main/java/org/example/factorial/Main.java
+++ src/main/java/org/example/factorial/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         int num = 5;
         Factorial f = new Factorial();
-        int recursive = f.factorial(5);
+        int recursive = f.factorial(num);
         System.out.println(recursive);
-        int iterative = f.findFactorial(5);
+        int iterative = f.findFactorial(num);
         System.out.println(iterative);
     }
 }
